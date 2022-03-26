7948772dad54c5b2c06fa3446f05401a93db4dde
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2022-03-26 11:04:08 +0200

    clear_color and circle are done.

--- CMP3060 Phase 1 - Student Version/assets/shaders/circle.frag
+++ CMP3060 Phase 1 - Student Version/assets/shaders/circle.frag
@@ -2,14 +2,19 @@
 
 out vec4 frag_color;
 
-//TODO: Define uniforms for the center and the radius
+//DONE: Define uniforms for the center and the radius
+uniform vec2 center;
+uniform float radius;
 
 uniform vec4 inside_color = vec4(1.0, 0.0, 0.0, 1.0);
 uniform vec4 outside_color = vec4(0.0, 0.0, 0.0, 1.0);
 
 void main(){
-    //TODO: Write code that will draw the circle
-    if(gl_FragCoord.x <= 256){
+    //DONE: Write code that will draw the circle
+    float distance = (gl_FragCoord.x - center.x) * (gl_FragCoord.x - center.x) + (gl_FragCoord.y - center.y) * (gl_FragCoord.y - center.y);
+    float radius_sq = radius * radius;
+
+    if(distance <= radius_sq){
         frag_color = inside_color;
     } else {
         frag_color = outside_color;
--- CMP3060 Phase 1 - Student Version/source/states/clear_color_state.hpp
+++ CMP3060 Phase 1 - Student Version/source/states/clear_color_state.hpp
@@ -2,19 +2,29 @@
 
 #include <application.hpp>
 
-class ClearColorState: public our::State {
+class ClearColorState : public our::State {
     // onInitialize() function is called once before the state starts
     void onInitialize() override {
-        //TODO: Read the color from the configuration file and use it to set the clear color for the window
-        //HINT: you can the configuration for the whole application using "getApp()->getConfig()"
-        //To see how the clear color is written in the json files, see "config/blue-screen.json"
-        //To know how read data from a nlohmann::json object, 
-        //look at the following documentation: https://json.nlohmann.me/features/element_access/
+        // DONE: Read the color from the configuration file and use it to set the clear color for the window
+        // HINT: you can the configuration for the whole application using "getApp()->getConfig()"
+        // To see how the clear color is written in the json files, see "config/blue-screen.json"
+        // To know how read data from a nlohmann::json object,
+        // look at the following documentation: https://json.nlohmann.me/features/element_access/
+        
+        //Getting the config file
+        nlohmann::json color_config = getApp()->getConfig();
+        //Extracting the color values
+        float r = color_config["scene"]["clear-color"]["r"];
+        float g = color_config["scene"]["clear-color"]["g"];
+        float b = color_config["scene"]["clear-color"]["b"];
+        float a = color_config["scene"]["clear-color"]["a"];
+        //Setting the color
+        glClearColor(r, g, b, a);
     }
 
-    // onDraw(deltaTime) function is called every frame 
+    // onDraw(deltaTime) function is called every frame
     void onDraw(double deltaTime) override {
-        //At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
+        // At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
         glClear(GL_COLOR_BUFFER_BIT);
     }
 };
