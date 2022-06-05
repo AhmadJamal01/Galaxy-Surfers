a857915d1a95a2778d03c86fedc120aa7c675a9f
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2022-06-05 07:14:41 +0200

    added collisionComponent
    In any json files used later on, you should a collision component and include the mesh for the entity for any object expected to collide. You can find an example of this in ship-collision-test.jsonc. The ship and asteroids have a collision component with their respective meshes

--- CMPN205 Phase 2+3 - Student Version/source/common/systems/collision-system.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/common/systems/collision-system.hpp
@@ -20,60 +20,75 @@ namespace our
 {
     class CollisionSystem
     {
-        // Application *app;              // The application in which the state runs (app holds general data, controlling events)
-        MeshRendererComponent *player; // We need external acess to the camera component of the player for ImGUI
-        MeshRendererComponent *collider;
+        // the player we will be checking for collisions
+        CollisionComponent *player;
+        // will store the entities that we will be checking for collisions
+        CollisionComponent *collider;
 
     public:
-        // This should be called every frame to update all entities containing a NinjaControllerComponent
+        // This should be called every frame to detect if the player and any other entity collide.
         void detectCollision(World *world)
         {
-            // test using camera and box
             for (auto entity1 : world->getEntities())
             {
-                // Look for the NinjaControllerComponent (has speed data so far)
-                // and the camera component (in the camera entity). Note that even the nested camera entity will be looped on (2nd if condition)
-                if (auto playerCandidate = entity1->getComponent<MeshRendererComponent>(); playerCandidate != nullptr && entity1->name == "player")
+                // Look for the player
+                if (auto playerCandidate = entity1->getComponent<CollisionComponent>(); playerCandidate != nullptr && entity1->name == "player")
                 {
                     player = playerCandidate;
-                    std::cout<<"Got the ship"<<'\n';
+                    // std::cout << "Got the ship" << '\n';
                 }
                 if (player)
                     break;
             }
             for (auto entity2 : world->getEntities())
             {
-                // if the entity is different from the current entity
-
-                // if (auto boxCandidate = entity2->getComponent<MeshRendererComponent>(); boxCandidate != nullptr)
-                if (auto boxCandidate = entity2->getComponent<MeshRendererComponent>(); boxCandidate != nullptr && entity2->name != "player")
+                // looping over all entities with collision components who are not the player
+                if (auto boxCandidate = entity2->getComponent<CollisionComponent>(); boxCandidate != nullptr && entity2->name != "player")
                 {
+                    // setting the collider as the current iterator
                     collider = boxCandidate;
+
+                    // get the owner entities of the collision components
                     Entity *myEntity1 = player->getOwner();
                     Entity *myEntity2 = collider->getOwner();
+                    // gets the name of the collider specified in the json file for testing
                     std::string name = myEntity2->name;
 
-                    glm::vec3 minCameraVertex = myEntity1->getComponent<MeshRendererComponent>()->mesh->minvertex;
-                    glm::vec3 maxCameraVertex = myEntity1->getComponent<MeshRendererComponent>()->mesh->maxvertex;
+                    // gets the min and max vertices using the mesh class
+                    glm::vec3 minCameraVertex = myEntity1->getComponent<CollisionComponent>()->mesh->minvertex;
+                    glm::vec3 maxCameraVertex = myEntity1->getComponent<CollisionComponent>()->mesh->maxvertex;
+                    // glm::vec4 minCameraVertex = glm::vec4(myEntity2->getComponent<CollisionComponent>()->mesh->minvertex, 1);
+                    // glm::vec4 maxCameraVertex = glm::vec4(myEntity2->getComponent<CollisionComponent>()->mesh->maxvertex, 1);
+
+                    // transforms the min and max vertices to the wold space
                     minCameraVertex *= myEntity1->localTransform.scale[0];
                     maxCameraVertex *= myEntity1->localTransform.scale[0];
                     minCameraVertex += myEntity1->localTransform.position;
                     maxCameraVertex += myEntity1->localTransform.position;
+                    // minCameraVertex = myEntity2->getLocalToWorldMatrix() * minCameraVertex;
+                    // maxCameraVertex = myEntity2->getLocalToWorldMatrix() * maxCameraVertex;
 
+                    // gets the min and max vertices using the mesh class
+                    glm::vec3 minCollider = myEntity2->getComponent<CollisionComponent>()->mesh->minvertex;
+                    glm::vec3 maxCollider = myEntity2->getComponent<CollisionComponent>()->mesh->maxvertex;
+                    // glm::vec4 minCollider = glm::vec4(myEntity2->getComponent<CollisionComponent>()->mesh->minvertex, 1);
+                    // glm::vec4 maxCollider = glm::vec4(myEntity2->getComponent<CollisionComponent>()->mesh->maxvertex, 1);
 
-                    glm::vec3 minCollider = myEntity2->getComponent<MeshRendererComponent>()->mesh->minvertex;
-                    glm::vec3 maxCollider = myEntity2->getComponent<MeshRendererComponent>()->mesh->maxvertex;
+                    // transforms the min and max vertices to the wold space
                     minCollider *= myEntity2->localTransform.scale[0];
                     maxCollider *= myEntity2->localTransform.scale[0];
                     minCollider += myEntity2->localTransform.position;
                     maxCollider += myEntity2->localTransform.position;
+                    // minCollider = myEntity2->getLocalToWorldMatrix() * minCollider;
+                    // maxCollider = myEntity2->getLocalToWorldMatrix() * maxCollider;
 
+                    // collision between AABBs check
                     if ((minCameraVertex.x <= maxCollider.x && maxCameraVertex.x >= minCollider.x) &&
                         (minCameraVertex.y <= maxCollider.y && maxCameraVertex.y >= minCollider.y) &&
                         (minCameraVertex.z <= maxCollider.z && maxCameraVertex.z >= minCollider.z))
                     {
                         i++;
-                        std::cout << "collision detected " << name << " " << i << std::endl;
+                        std::cout << "collision detected with " << name << " " << i << std::endl;
                     }
                 }
             }
